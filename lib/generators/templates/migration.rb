e915d43be207d718ea44f67ff3a617349257bcb0
James A. Rosen <james@zendesk.com>
2010-10-15 08:49:46 -0700

    added migration generator

---
+++ lib/generators/templates/migration.rb
@@ -0,0 +1,15 @@
+class CreateFeatures < ActiveRecord::Migration
+  def self.up
+    create_table :features do |t|
+      t.string   :name, :null => false
+      t.integer  :deployment_percentage, :null => false
+      #Any additional fields here
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :features
+  end
+end
