40cad758a2bca202c22fab7ed03d7cb2e002b013
James A. Rosen <james@zendesk.com>
2010-10-17 19:49:14 -0700

    extracted out deployment_percentage range and output generation into FeaturesHelper

--- app/helpers/arturo/features_helper.rb
+++ app/helpers/arturo/features_helper.rb
@@ -20,8 +20,23 @@ module Arturo
       end
     end
 
-    def range_tag(name, value, options = {})
-      tag(:input, { 'type' => 'range', 'name' => name, 'id' => sanitize_to_id(name), 'value' => value }.update(options.stringify_keys))
+    def deployment_percentage_range_and_output_tags(name, value, options = {})
+      id = sanitize_to_id(name)
+      options = {
+        'type' => 'range',
+        'name' => name,
+        'id' => id,
+        'value' => value,
+        'min' => '0',
+        'max' => '100',
+        'step' => '1',
+        'class' => 'deployment_percentage'
+      }.update(options.stringify_keys)
+      tag(:input, options) + deployment_percentage_output_tag(id, value)
+    end
+
+    def deployment_percentage_output_tag(id, value)
+      content_tag(:output, value, { 'for' => id, 'class' => 'deployment_percentage no_js' })
     end
 
     def error_messages_for(feature, attribute)
