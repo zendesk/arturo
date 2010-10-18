b13e78578ab30fce7c43d91966166de8549e80b5
James A. Rosen <james@zendesk.com>
2010-10-17 17:45:02 -0700

    added some styling to index and form pages

--- lib/generators/arturo/assets_generator.rb
+++ lib/generators/arturo/assets_generator.rb
@@ -7,8 +7,9 @@ module Arturo
     end
 
     def copy_assets
-      copy_file 'arturo.css',  'public/stylesheets/arturo.css', :force => true
+      copy_file 'arturo.css',                 'public/stylesheets/arturo.css', :force => true
       copy_file 'arturo_customizations.css',  'public/stylesheets/arturo_customizations.css', :skip => true
+      copy_file 'semicolon.png',              'public/images/semicolon.png'
     end
 
   end
