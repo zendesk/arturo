5ca6cd3ddd22f14ee7168520b431c9a1a8b688bb
James A. Rosen <james@zendesk.com>
2010-10-24 19:43:35 -0700

    added some tests about mixin methods not becoming actions

--- test/dummy_app/test/unit/engine_test.rb
+++ test/dummy_app/test/unit/engine_test.rb
@@ -6,8 +6,21 @@ class EngineTest < ActiveSupport::TestCase
     assert ActionController::Base < Arturo::FeatureAvailability
   end
 
+  def test_feature_availability_methods_are_not_actions
+    assert !BooksController.action_methods.include?('feature_enabled?')
+    assert !BooksController.action_methods.include?('if_feature_enabled')
+  end
+
   def test_feature_availability_is_a_helper
     assert Arturo::FeaturesController._helpers < Arturo::FeatureAvailability
+  end
+
+  def test_controllers_include_filters
+    assert ActionController::Base < Arturo::ControllerFilters
+  end
+
+  def test_controller_filter_methods_are_not_actions
+    assert !BooksController.action_methods.include?('on_feature_disabled')
   end
 
 end
