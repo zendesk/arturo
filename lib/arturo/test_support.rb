8d16bc0300158f9a563972260bf9b082bdf5a6f3
James A. Rosen <james@zendesk.com>
2010-11-03 19:28:31 -0700

    added arturo/test_support that adds Arturo.enable_feature! and .disable_feature! (fixes #10)

---
+++ lib/arturo/test_support.rb
@@ -0,0 +1,30 @@
+Arturo.class_eval do
+
+  # Enable a feature; create it if necessary.
+  # For use in testing. Not auto-required on load. To load,
+  #
+  #   require 'arturo/test_support'
+  #
+  # @param [Symbol, String] name the feature name
+  def enable_feature!(name)
+    feature = Arturo::Feature.find_by_symbol(name)
+    if feature
+      feature.update_attributes(:deployment_percentage => 100)
+    else
+      Arturo::Feature.create(:symbol => name, :deployment_percentage => 100)
+    end
+  end
+
+  # Disable a feature if it exists.
+  # For use in testing. Not auto-required on load. To load,
+  #
+  #   require 'arturo/test_support'
+  #
+  # @param [Symbol, String] name the feature name
+  def disable_feature!(name)
+    if (feature = Arturo::Feature.find_by_symbol(name))
+      feature.update_attributes(:deployment_percentage => 0)
+    end
+  end
+
+end
