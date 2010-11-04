4a942364b3cf64803fb93e63bcc015a8baca980b
James A. Rosen <james@zendesk.com>
2010-11-03 18:36:46 -0700

    added a User model to the dummy app so it's usable in a browser

--- test/dummy_app/app/controllers/application_controller.rb
+++ test/dummy_app/app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   layout 'application'
+
+  def current_user
+    User.new(:name => 'Freddykins', :admin => true)
+  end
 end
