4fec63fffe7fc4b98f2fc12f54f02d280209b86f
Luke van der Hoeven <hungerandthirst@gmail.com>
2010-12-13 11:54:37 -0500

    tests for error messages helper w/ html safe test.

--- test/dummy_app/test/unit/features_helper_test.rb
+++ test/dummy_app/test/unit/features_helper_test.rb
@@ -3,8 +3,37 @@ require 'arturo/features_helper'
 
 class ArturoFeaturesHelperTest < ActiveSupport::TestCase
 
-  def test_truth
-    assert true
-  end
+  ERBHandler = ActionView::Template::Handlers::ERB
 
+  class Context
+    include Arturo::FeaturesHelper
+    attr_accessor :_template, :output_buffer
+
+    def initialize
+      @output_buffer = "original"
+      @_virtual_path = nil
+    end
+
+    def bad_feature
+      feature = Factory(:feature)  
+      feature.deployment_percentage = 101
+      feature.valid?
+      return feature
+    end
+  end
+  
+  def test_error_messages_for
+    output = "<ul class=\"errors\"><li class=\"error\">must be less than or equal to 100</li></ul>"
+
+    template_result = ActionView::Template.new(
+      "<%= error_messages_for(bad_feature, :deployment_percentage) %>",
+      "partial",
+      ERBHandler,
+      :virtual_path => "partial"
+    )
+
+    assert_equal output, template_result.render( Context.new, {} )
+    assert template_result.render( Context.new, {} ).html_safe?
+  
+  end
 end
