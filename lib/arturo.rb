04d3f66675ff4d31580ba82fca29c69353ab9c8e
James A. Rosen <james@zendesk.com>
2011-02-13 16:30:06 -0800

    syntactic sugar for availability check [#14]

--- lib/arturo.rb
+++ lib/arturo.rb
@@ -7,13 +7,30 @@ module Arturo
   require 'arturo/range_form_support'
   require 'arturo/engine' if defined?(Rails) && Rails::VERSION::MAJOR == 2 && Rails::VERSION::MINOR == 3
 
-  # Quick check for whether a feature is enabled for a recipient.
-  # @param [String, Symbol] feature_name
-  # @param [#id] recipient
-  # @return [true,false] whether the feature exists and is enabled for the recipient
-  def self.feature_enabled_for?(feature_name, recipient)
-    f = self::Feature.to_feature(feature_name)
-    f && f.enabled_for?(recipient)
-  end
+  class <<self
 
+    # Quick check for whether a feature is enabled for a recipient.
+    # @param [String, Symbol] feature_name
+    # @param [#id] recipient
+    # @return [true,false] whether the feature exists and is enabled for the recipient
+    def feature_enabled_for?(feature_name, recipient)
+      f = self::Feature.to_feature(feature_name)
+      f && f.enabled_for?(recipient)
+    end
+
+    ENABLED_FOR_METHOD_NAME = /^(\w+)_enabled_for\?$/
+
+    def respond_to?(symbol)
+      symbol.to_s =~ ENABLED_FOR_METHOD_NAME || super(symbol)
+    end
+
+    def method_missing(symbol, *args, &block)
+      if (args.length == 1 && match = ENABLED_FOR_METHOD_NAME.match(symbol.to_s))
+        feature_enabled_for?(match[1], args[0])
+      else
+        super(symbol, *args, &block)
+      end
+    end
+
+  end
 end
