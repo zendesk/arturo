96ca4857470206f723d1adfb0d57c36c84630fff
James A. Rosen <james@zendesk.com>
2011-11-01 09:00:40 -0700

    Some improvements to the dummy app's User
     * add an ID so it quacks more like an AR object
     * add puts and inspect for debugging

--- test/dummy_app/app/models/user.rb
+++ test/dummy_app/app/models/user.rb
@@ -1,14 +1,24 @@
 class User
 
-  attr_reader :name
+  attr_reader :name, :id
 
   def initialize(options = {})
     @name = options[:name]
     @admin = options[:admin]
+    @id = options[:id]
   end
 
   def admin?
     !!@admin
+  end
+
+  def to_s
+    name
+  end
+
+  def inspect
+    type = @admin ? 'Admin' : 'User'
+    "<Mock #{type} ##{id}: #{name}>"
   end
 
 end
