5323e6ea54eef6f0e4e2505123307b02862d58f4
grosser <grosser.michael@gmail.com>
2013-03-01 10:49:34 -0800

    cleanup and silence

--- test/dummy_app/test/prepare_database.rb
+++ test/dummy_app/test/prepare_database.rb
@@ -1,8 +1,9 @@
 require 'active_record'
 
-ActiveRecord::Base.establish_connection({
+ActiveRecord::Base.establish_connection(
   :adapter  => 'sqlite3',
   :database => ':memory:'
-})
+)
 
+ActiveRecord::Schema.verbose = false
 require File.expand_path('../../db/schema.rb', __FILE__)
