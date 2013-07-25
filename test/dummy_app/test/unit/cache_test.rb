e0eac24da47cd63df73b4ee6d7e35a5296db9817
grosser <grosser.michael@gmail.com>
2013-07-25 08:50:20 -0700

    use minitest-spec for later describe block usage

--- test/dummy_app/test/unit/cache_test.rb
+++ test/dummy_app/test/unit/cache_test.rb
@@ -1,8 +1,7 @@
 require File.expand_path('../../test_helper', __FILE__)
 require 'arturo/features_helper'
 
-class CacheTest < ActiveSupport::TestCase
-
+describe "caching" do
   Arturo::Feature.extend(Arturo::FeatureCaching)
 
   class StupidCache
@@ -19,60 +18,30 @@ class CacheTest < ActiveSupport::TestCase
     end
   end
 
-  def setup
+  before do
     @feature = create(:feature)
     Arturo::Feature.cache_ttl = 30.minutes
-    Arturo::Feature.feature_cache.clear
+    Arturo::Feature.feature_cache = Arturo::FeatureCaching::Cache.new
   end
 
-  def teardown
+  after do
     Arturo::Feature.cache_ttl = 0 # turn off for other tests
     Timecop.return
   end
 
-  def test_first_load_hits_database
+  it "hits db on first load" do
     Arturo::Feature.expects(:where).once.returns([@feature.reload])
     Arturo::Feature.to_feature(@feature.symbol)
   end
 
-  def test_subsequent_loads_within_ttl_hit_cache
+  it "hits cache on subsequent loads within ttl" do
     Arturo::Feature.expects(:where).once.returns([@feature.reload])
     Arturo::Feature.to_feature(@feature.symbol)
     Arturo::Feature.to_feature(@feature.symbol)
     Arturo::Feature.to_feature(@feature.symbol)
   end
 
-  def test_no_such_feature_is_cached
-    Arturo::Feature.expects(:where).once.returns([])
-    assert_kind_of Arturo::NoSuchFeature, Arturo::Feature.to_feature(:ramen)
-    assert_kind_of Arturo::NoSuchFeature, Arturo::Feature.to_feature(:ramen)
-    assert_kind_of Arturo::NoSuchFeature, Arturo::Feature.to_feature(:ramen)
-  end
-
-  def test_works_with_other_cache_backend
-    Arturo::Feature.feature_cache = StupidCache.new
-    Arturo::Feature.expects(:where).once.returns([@feature.reload])
-    Arturo::Feature.to_feature(@feature.symbol.to_sym)
-    Arturo::Feature.to_feature(@feature.symbol)
-    Arturo::Feature.to_feature(@feature.symbol.to_sym)
-    Arturo::Feature.to_feature(@feature.symbol)
-  end
-
-  def test_clear_cache
-    Arturo::Feature.to_feature(@feature.symbol)
-    Arturo::Feature.feature_cache.clear
-    Arturo::Feature.expects(:where).once.returns([@feature.reload])
-    Arturo::Feature.to_feature(@feature.symbol)
-  end
-
-  def test_turn_off_caching
-    Arturo::Feature.cache_ttl = 0
-    Arturo::Feature.expects(:where).twice.returns([@feature.reload])
-    Arturo::Feature.to_feature(@feature.symbol)
-    Arturo::Feature.to_feature(@feature.symbol)
-  end
-
-  def test_ttl_expiry
+  it "does not hit cache for subsequent loads outside of ttl" do
     Arturo::Feature.to_feature(@feature.symbol)
     Arturo::Feature.expects(:where).once.returns([@feature.reload])
     Timecop.travel(Time.now + Arturo::Feature.cache_ttl - 5.seconds)
@@ -81,7 +50,37 @@ class CacheTest < ActiveSupport::TestCase
     Arturo::Feature.to_feature(@feature.symbol)
   end
 
-  def test_warm_cache
+  it "caches missing features" do
+    Arturo::Feature.expects(:where).once.returns([])
+    assert_kind_of Arturo::NoSuchFeature, Arturo::Feature.to_feature(:ramen)
+    assert_kind_of Arturo::NoSuchFeature, Arturo::Feature.to_feature(:ramen)
+    assert_kind_of Arturo::NoSuchFeature, Arturo::Feature.to_feature(:ramen)
+  end
+
+  it "works with other cache backends" do
+    Arturo::Feature.feature_cache = StupidCache.new
+    Arturo::Feature.expects(:where).once.returns([@feature.reload])
+    Arturo::Feature.to_feature(@feature.symbol.to_sym)
+    Arturo::Feature.to_feature(@feature.symbol)
+    Arturo::Feature.to_feature(@feature.symbol.to_sym)
+    Arturo::Feature.to_feature(@feature.symbol)
+  end
+
+  it "can clear the cache" do
+    Arturo::Feature.to_feature(@feature.symbol)
+    Arturo::Feature.feature_cache.clear
+    Arturo::Feature.expects(:where).once.returns([@feature.reload])
+    Arturo::Feature.to_feature(@feature.symbol)
+  end
+
+  it "can turn off caching" do
+    Arturo::Feature.cache_ttl = 0
+    Arturo::Feature.expects(:where).twice.returns([@feature.reload])
+    Arturo::Feature.to_feature(@feature.symbol)
+    Arturo::Feature.to_feature(@feature.symbol)
+  end
+
+  it "can warm the cache" do
     second_feature = create(:feature)
     Arturo::Feature.warm_cache!
 
