f6b04c2bb993bf5b49f1e8874d01a8f435bcecae
James A. Rosen <james@zendesk.com>
2014-06-26 19:42:13 -0400

    Arturo: better respond_to? handling
     1. allow a second, optional argument for `Arturo.respond_to?`
     2. also define `Arturo.respond_to_missing?` for `.method` support
     3. emit a deprecation warning when calling
        `Arturo.respond_to?` with `FEATURE_enabled_for?`

--- lib/arturo.rb
+++ lib/arturo.rb
@@ -19,13 +19,24 @@ module Arturo
 
     ENABLED_FOR_METHOD_NAME = /^(\w+)_enabled_for\?$/
 
-    def respond_to?(symbol)
-      symbol.to_s =~ ENABLED_FOR_METHOD_NAME || super(symbol)
+    ENABLED_FOR_METHOD_NAME_WARNING = "Arturos FEATURE_enabled_for?(OBJECT) methods are going away, use feature_enabled_for?(FEATURE, OBJECT)"
+
+    def respond_to_missing?(symbol, include_all = false)
+      if symbol.to_s =~ ENABLED_FOR_METHOD_NAME
+        warn ENABLED_FOR_METHOD_NAME_WARNING
+        true
+      else
+        super
+      end
+    end
+
+    def respond_to?(symbol, include_all = false)
+      respond_to_missing? symbol, include_all
     end
 
     def method_missing(symbol, *args, &block)
       if (args.length == 1 && match = ENABLED_FOR_METHOD_NAME.match(symbol.to_s))
-        warn "Arturos FEATURE_enabled_for?(OBJECT) methods are going away, use feature_enabled_for?(FEATURE, OBJECT)"
+        warn ENABLED_FOR_METHOD_NAME_WARNING
         feature_enabled_for?(match[1], args[0])
       else
         super(symbol, *args, &block)
