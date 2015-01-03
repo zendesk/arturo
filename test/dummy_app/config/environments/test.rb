f8eb7e5364fdea2787fe1f0bc4673845b0a1bb55
Benjamin Quorning <bquorning@zendesk.com>
2015-01-03 11:39:54 +0100

    Test against Rails 4.2

--- test/dummy_app/config/environments/test.rb
+++ test/dummy_app/config/environments/test.rb
@@ -34,4 +34,6 @@ DummyApp::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
+
+  config.active_support.test_order = :random
 end
