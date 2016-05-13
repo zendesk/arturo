a380279d72cf9f54ed9f72f6f34ab5cafab04fa5
Pierre Schambacher <pschambacher@zendesk.com>
2016-05-13 11:33:54 -0700

    Simplify dummy application

--- test/dummy_app/config/initializers/session_store.rb
+++ test/dummy_app/config/initializers/session_store.rb
@@ -1,9 +1,2 @@
 # frozen_string_literal: true
-# Be sure to restart your server when you modify this file.
-
 DummyApp::Application.config.session_store :cookie_store, :key => '_dummy_app_session'
-
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rake db:sessions:create")
-# DummyApp::Application.config.session_store :active_record_store
