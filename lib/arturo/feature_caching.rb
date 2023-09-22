c86c9ad65d5e3dddccf3de003ed3f767900cec6e
Jury Razumau <jury.razumau@zendesk.com>
2023-09-22 12:26:29 +0200

    build cache hash using each_with_object
    Instead of creating a temporary array with all Arturos only to convert it  into a hash immediately, we should be building this hash directly.

--- lib/arturo/feature_caching.rb
+++ lib/arturo/feature_caching.rb
@@ -102,7 +102,7 @@ module Arturo
         #   without cache extension option
         #
         def arturos_from_origin(fallback:)
-          Hash[Arturo::Feature.all.map { |f| [f.symbol.to_sym, f] }]
+          Arturo::Feature.all.each_with_object({}) { |f, hash| hash[f.symbol.to_sym] = f }
         rescue ActiveRecord::ActiveRecordError
           raise unless Arturo::Feature.extend_cache_on_failure?
 
