1776405b3e0127a1f813cd117ea602c71483dee2
Jury Razumau <jury.razumau@zendesk.com>
2024-04-15 11:26:03 +0200

    load non-controller modules from outside engine
    Apps that do not use the `FeaturesController` still need to load `FeatureAvailability` and `ControllerFilters` modules (and include them into other controllers). Instead of setting up a hook for loading them as part of the Rails engine, we should be doing this in the root `Arturo` file. This way, apps that don’t use the engine will still load these modules but will not run into issues with autoloading and eager loading caused by the engine.

--- lib/arturo.rb
+++ lib/arturo.rb
@@ -6,6 +6,7 @@ require_relative 'arturo/feature_methods'
 require_relative 'arturo/feature_availability'
 require_relative 'arturo/feature_management'
 require_relative 'arturo/feature_caching'
+require_relative 'arturo/controller_filters'
 
 module Arturo
   class << self
@@ -27,5 +28,14 @@ module Arturo
     def logger
       @logger || NullLogger.new
     end
+  end
+end
+
+ActiveSupport.on_load(:action_controller) do
+  include Arturo::FeatureAvailability
+  include Arturo::ControllerFilters
+  if respond_to?(:helper)
+    helper Arturo::FeatureAvailability
+    helper Arturo::FeatureManagement
   end
 end
